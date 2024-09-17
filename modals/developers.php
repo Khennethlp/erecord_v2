c87d765029cd704ecfad1797c45bdc5064f032b7
khenneth <khennethlp@gmail.com>
2024-09-17 13:22:36 +0800

    modified

--- modals/developers.php
+++ modals/developers.php
@@ -3,25 +3,25 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title letter-spacing text-bold" id="exampleModalLabel">DEVELOPERS</h5>
+        <h5 class="modal-title letter-spacing text-bold" id="exampleModalLabel"><i class="fas fa-users"></i>&nbsp; DEVELOPERS</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
         <div class="col-md-12">
-            <div class="col-md-6">
-                <label class="label">Version 1:</label>&nbsp;<code>2023</code>
+            <div class="col-md-12">
+            <i class="fas fa-code-branch text-lg text-secondary"></i>&nbsp;&nbsp;<label class="label">Version 1:</label>&nbsp;<code>2023</code>
                 <p class="devs">
                     EJ Montañano
                     <br>
                     Jane Irish Bautista
                 </p>
             </div>
+            <hr>
             <div class="col-md-6">
-                <label class="label">Version 2:</label>&nbsp;<code>2024</code>
+            <i class="fas fa-code-branch text-lg text-secondary"></i>&nbsp;&nbsp;<label class="label">Version 2:</label>&nbsp;<code>2024</code>
                 <p class="devs">Khenneth Puerto</p>
-                
             </div>
         </div>
       </div>
