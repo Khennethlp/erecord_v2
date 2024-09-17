2018518531358892372cc267dafabb36be0c1294
khenneth <khennethlp@gmail.com>
2024-09-17 08:39:56 +0800

    modify logout

--- modals/logout.php
+++ modals/logout.php
@@ -1,19 +1,20 @@
 <!-- Modal -->
 <form action="" method="post">
 <div class="modal fade" id="logout-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-center modal-sm" role="document">
     <div class="modal-content">
-      <div class="modal-header">     
+      <!-- <div class="modal-header">     
         <h5 class="modal-title" id="exampleModalLabel"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" >
           <span aria-hidden="true">&times;</span>
         </button>
-      </div>
+      </div> -->
       <div class="modal-body">
-                <h5 style="text-align:center;">Confirm Logout</h5>
+        <h5 style="text-align:center; text-transform: uppercase; font-size: 20px;">Confirm Logout</h5>
       </div>
       <div class="modal-footer">
-       <input type="submit" value="Logout" class="btn btn-danger col-sm-12" name="Logout">
+        <input type="submit" value="Logout" class="btn btn-danger col-sm-12" name="Logout">
+        <input type="button" value="Cancel" class="btn btn-muted col-sm-12" name="Cancel" data-dismiss="modal" aria-label="Close" style="background: #ECECE9;">
       </div>
     
     </div>
