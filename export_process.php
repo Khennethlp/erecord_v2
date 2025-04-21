27d16c89963ded439b8a434f8cfba16a1a78f728
Khenneth <167261858+Khennethlp@users.noreply.github.com>
2025-04-21 08:35:14 +0800

    fixed status to approved

--- export_process.php
+++ export_process.php
@@ -42,7 +42,7 @@ if ($category == 'Final') {
 
 $query .= "LEFT JOIN t_employee_m b ON a.emp_id = b.emp_id AND a.batch = b.batch
 JOIN m_process c ON a.process = c.process
-WHERE (a.i_status = 'Approved' OR a.i_status = 'Pending')";
+WHERE (a.i_status = 'Approved' OR a.i_status = 'Approved')";
 
 $conditions = [];
 if (!empty($emp_id)) {
